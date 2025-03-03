2ece4465849b3bcdef7fb342f261a9e3cb85476f
SATHISH S <sathishs.webdev@gmail.com>
2025-03-03 15:58:00 +0530

    fix: HomePage component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import PaginatedTable from "@/components/home/PaginatedTable";
 import StockCard from "@/components/home/StockCard";
 import SectionWrapper from "@/components/ui/SectionWrapper";
-import { SectionTopbar } from "@/components/home/SectionTopbar";
+import { SectionTopbar } from "@/components/home/SectionTopBar";
 import {
   useGetCandlestickDataQuery,
   useGetStocksListQuery,
